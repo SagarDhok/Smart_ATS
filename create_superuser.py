93b9ad34fb00a7f457ff2c8839b326e58daf8451
sagardhok <sdhok041@gmail.com>
2025-12-13 11:53:03 +0530

    Add auto superuser creation for Render free deploy

--- create_superuser.py
+++ create_superuser.py
@@ -8,16 +8,17 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
 email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
 password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
-if username and password:
-    if not User.objects.filter(username=username).exists():
-        User.objects.create_superuser(username, email, password)
+if email and password:
+    if not User.objects.filter(email=email).exists():
+        User.objects.create_superuser(
+            email=email,
+            password=password
+        )
         print("✅ Superuser created")
     else:
         print("ℹ️ Superuser already exists")
 else:
     print("❌ Missing environment variables")
-
