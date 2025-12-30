2ccfcac8b9fd4a4f4466040e47de9432797e01d2
sagardhok <sdhok041@gmail.com>
2025-12-30 08:11:18 +0530

    Update

--- users/models.py
+++ users/models.py
@@ -1,4 +1,3 @@
-
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import AbstractUser, BaseUserManager
@@ -9,9 +8,9 @@ import uuid
 # --------------------------
 # Custom User Manager
 # --------------------------
-class UserManager(BaseUserManager):
+class UserManager(BaseUserManager):   
     use_in_migrations = True
-    def create_user(self, email, password=None, **extra_fields):
+    def create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError("Email is required")
         email = self.normalize_email(email) #lowercase and strip etc
@@ -19,6 +18,7 @@ class UserManager(BaseUserManager):
         user.set_password(password)   # HASH PASSWORD
         user.save(using=self._db)
         return user
+    
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
@@ -27,20 +27,15 @@ class UserManager(BaseUserManager):
         return self.create_user(email, password, **extra_fields)
 
 
-#I built a custom UserManager because I removed the username field and switched the authentication to be fully email-based.
-# Django’s default UserManager still expects a username, so it cannot create or validate users correctly with my custom user model.
-# By overriding create_user() and create_superuser(), I ensured:
-# • email is used as the primary identifier
-# • passwords are always hashed using set_password()
-# • superuser flags (is_staff, is_superuser, and my custom role) are set correctly
-# Without a custom manager, Django’s authentication system simply wouldn’t work with an email-only user model.”**
-
 
 # --------------------------
 # Custom User Model
 # --------------------------
 class User(AbstractUser):
     username = None
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
     ROLE_CHOICES = [
         ("SUPERUSER", "SuperUser"),
         ("ADMIN", "Admin"),
@@ -48,19 +43,14 @@ class User(AbstractUser):
     ]
 
 
- 
-
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     role = models.CharField(max_length=20, choices=ROLE_CHOICES, default="HR")
-    company = models.CharField(max_length=255, null=True, blank=True)  
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     must_change_password = models.BooleanField(default=True)
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = []
     objects = UserManager()
 
     def __str__(self):
